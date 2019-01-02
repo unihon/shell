e432cd2bd51fd842e2a487d3ca579958fb4e2c02
unihon <unihon@outlook.com>
2019-01-02 20:40:48 +0800

    support the windows uri with space

--- wcp/wcp.sh
+++ wcp/wcp.sh
@@ -6,19 +6,24 @@
 #------------------------------------
 #	Usage:
 #	./wcp.sh "C:\xx\xx"
+#	./wcp.sh "C:\xx\x x"
 #------------------------------------
 #	Author:
 #	https://unihon.github.io/
 #------------------------------------
-#	Update:
+#	Date:
 #	2018-12-27
+#	Update:
+#	2019-01-02
 #===================================
 
 str=$1
 
-row=${str//\\/\/}
+tmp=${str//\\/\/}
+row=${tmp//\ /\\ }
+
 typeset -l partition=$(echo $row|cut -d ":" -f1)
 uri=$(echo $row|cut -d ":" -f2)
 uri_l="/mnt/"${partition}${uri}
 
-cp -r $uri_l .
+echo $uri_l|xargs -i cp -r {} .
